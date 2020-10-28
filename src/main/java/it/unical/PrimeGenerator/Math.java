460fec5af8b79f560f53e93fa891e4809fed8f75
Debora <dbrippolito@gmail.com>
2020-10-28 13:07:25 +0100

    Second Commit for test cases

--- src/main/java/it/unical/PrimeGenerator/Math.java
+++ src/main/java/it/unical/PrimeGenerator/Math.java
@@ -7,8 +7,10 @@ public class Math {
 	
 	public List<Integer> primeNumbersGenerator(int n){
 		List<Integer> primeNumbers = new LinkedList<>();
-		if(n>=2)
+		if(n>=2) {
 			primeNumbers.add(2);
+			System.out.println("Modifica");
+		}
 		for(int i = 3; i<=n; i += 2) {
 			if(isPrime(i))
 				primeNumbers.add(i);
@@ -16,10 +18,10 @@ public class Math {
 		return primeNumbers;
 	}
 
-	private boolean isPrime(int number) {
+	public boolean isPrime(int number) {
 		if(number<2)
 			return false;
-		for(int i = 2; i*i<number; i++) {
+		for(int i = 2; i*i<=number; i++) {
 			if(number%i == 0)
 				return false;
 		}
