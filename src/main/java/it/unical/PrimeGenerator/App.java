4c2daae40e371f373cd3c21254421234460c4133
Debora <you@example.com>
2020-10-28 11:41:45 +0100

    Initial Commit

---
+++ src/main/java/it/unical/PrimeGenerator/App.java
@@ -0,0 +1,14 @@
+package it.unical.PrimeGenerator;
+
+/**
+ * Test Math function
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Math m = new Math();
+        System.out.println(m.primeNumbersGenerator(20));
+    }
+}
---
+++ src/main/java/it/unical/PrimeGenerator/Math.java
@@ -0,0 +1,29 @@
+package it.unical.PrimeGenerator;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Math {
+	
+	public List<Integer> primeNumbersGenerator(int n){
+		List<Integer> primeNumbers = new LinkedList<>();
+		if(n>=2)
+			primeNumbers.add(2);
+		for(int i = 3; i<=n; i += 2) {
+			if(isPrime(i))
+				primeNumbers.add(i);
+		}
+		return primeNumbers;
+	}
+
+	private boolean isPrime(int number) {
+		if(number<2)
+			return false;
+		for(int i = 2; i*i<number; i++) {
+			if(number%i == 0)
+				return false;
+		}
+		return true;
+	}
+
+}
